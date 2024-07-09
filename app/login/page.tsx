d0f53453d864fcd8ca8f547f0d40c4a79ef4b958
lylaw27 <lylaw27@gmail.com>
2024-07-09 18:44:31 +0800

    2506

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,13 +2,84 @@ import { login, signup } from './actions'
 
 export default function LoginPage() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
+    <div className="min-h-screen p-4 bg-gray-100 lg:justify-center">
+      <div
+        className="flex flex-col overflow-hidden bg-white rounded-md shadow-lg max md:flex-row md:flex-1 lg:max-w-screen-md"
+      >
+        <div className="p-5 bg-white md:flex-1">
+          <h3 className="my-4 text-2xl font-semibold text-gray-700">Account Login</h3>
+          <form action="#" className="flex flex-col space-y-5">
+            <div className="flex flex-col space-y-1">
+              <label htmlFor="email" className="text-sm font-semibold text-gray-500">Email address</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                required
+                autoFocus 
+                className="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
+              />
+            </div>
+            <div className="flex flex-col space-y-1">
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="text-sm font-semibold text-gray-500">Password</label>
+                <a href="#" className="text-sm text-blue-600 hover:underline focus:text-blue-800">Forgot Password?</a>
+              </div>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                required
+                className="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
+              />
+            </div>
+            <div className="flex items-center space-x-2">
+              <input
+                type="checkbox"
+                id="remember"
+                className="w-4 h-4 transition duration-300 rounded focus:ring-2 focus:ring-offset-0 focus:outline-none focus:ring-blue-200"
+              />
+              <label htmlFor="remember" className="text-sm font-semibold text-gray-500">Remember me</label>
+            </div>
+            <div>
+              <button
+                formAction={signup}
+                type="submit"
+                className="w-full px-4 py-2 text-lg font-semibold text-white transition-colors duration-300 bg-blue-500 rounded-md shadow hover:bg-blue-600 focus:outline-none focus:ring-blue-200 focus:ring-4"
+              >
+                Sign Up
+              </button>
+            </div>
+            <div>
+              <button
+                formAction={login}
+                type="submit"
+                className="w-full px-4 py-2 text-lg font-semibold text-white transition-colors duration-300 bg-blue-500 rounded-md shadow hover:bg-blue-600 focus:outline-none focus:ring-blue-200 focus:ring-4"
+              >
+                Log in
+              </button>
+            </div>
+            <div className="flex flex-col space-y-5">
+              <span className="flex items-center justify-center space-x-2">
+                <span className="h-px bg-gray-400 w-14"></span>
+                <span className="font-normal text-gray-500">or login with</span>
+                <span className="h-px bg-gray-400 w-14"></span>
+              </span>
+              <div className="flex flex-col space-y-4">
+                <a
+                  href="#"
+                  className="flex items-center justify-center px-4 py-2 space-x-2 transition-colors duration-300 border border-gray-800 rounded-md group hover:bg-gray-800 focus:outline-none"
+                >
+                  <span>
+                  <svg width="20" height="20" viewBox="-3 0 262 262" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid"><path d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027" fill="#4285F4"/><path d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055-34.523 0-63.824-22.773-74.269-54.25l-1.531.13-40.298 31.187-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1" fill="#34A853"/><path d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82 0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602l42.356-32.782" fill="#FBBC05"/><path d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0 79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251" fill="#EB4335"/></svg>
+                  </span>
+                  <span className="text-sm font-medium text-gray-800 group-hover:text-white">Google</span>
+                </a>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
