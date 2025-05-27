94500bae137e85cd9c8155e18a192b654cb2760d
Larry Law <69749219+lylaw27@users.noreply.github.com>
2025-05-27 19:59:03 +0800

    Update ats-section.tsx

--- components/ats-section.tsx
+++ components/ats-section.tsx
@@ -10,7 +10,7 @@ export default function ATSSection(){
                 <div className="flex h-auto p-10">
                     <video src="/ats-video.mov" width="320" controls autoPlay muted/>
                 </div>
-                <Button as={Link}  href="/atspage" className="bg-ruby text-3xl p-9 text-white font-black">
+                <Button as={Link}  href="/atsscan" className="bg-ruby text-3xl p-9 text-white font-black">
                 立即體驗
                 </Button>
             </div>
